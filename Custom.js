3de67b7ac549d456a28b91e562ad76674162c4cc
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-23 20:33:50 +0000

    Initial commit

---
+++ Custom.js
@@ -0,0 +1,63 @@
+/**
+ * Created by SamChaudry on 23/03/2017.
+ */
+
+$( document ).ready(function() {
+
+
+
+});
+
+
+
+$("#postData").click(function(){
+
+    var jsonObject = {};
+
+    var inputDataArray = ($(".inputData").serializeArray());
+
+    for (var i = 0; i < inputDataArray.length; i++){
+
+        jsonObject[inputDataArray[i]['name']] = inputDataArray[i]['value'];
+
+    }
+
+
+     var jsonObject = {};
+
+     var inputDataArray = ($(".inputData").serializeArray());
+
+     for (var i = 0; i < inputDataArray.length; i++){
+
+     jsonObject[inputDataArray[i]['name']] = inputDataArray[i]['value'];
+
+     }
+
+
+     var data = JSON.stringify(jsonObject);
+
+     alert(data);
+     console.log(data);
+
+     //Data structure
+    //{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
+
+    $.ajax({
+
+        url: 'services.php',
+        type: 'post',
+        data: {"points" : JSON.stringify(data)},
+        success: function(data) {
+
+        // Do something with data that came back.
+
+       alert(data);
+
+    }
+
+});
+
+
+
+});
+
---
+++ services.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST["points"])) {
+
+    // Decode our JSON into PHP objects we can use
+
+    $points = json_decode($_POST["points"]);
+
+    // Access our object's data and array values.
+
+    echo "Data is: " . $points->data . "<br>";
+
+    echo "Values" . $points->arPoints[0]->x . ", " . $points->arPoints[0]->y;
+
+
+    //Extract values and write to variables
+}
