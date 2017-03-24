21ece43575c36debf6f8b481b4f20b1c643d0507
0805594 <a@a.com>
2017-03-24 14:49:02 +0000

    echo changes

--- services.php
+++ services.php
@@ -2,12 +2,14 @@
 if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
-
+    //Pulling
     $points = json_decode($_POST["points"]);
 
 
     echo $_POST['points'];
 
+
+    echo ($points); 
 
     // Access our object's data and array values.
 
