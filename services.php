d23cf659fbf1a6da8681300263fc60d2987bfba1
0805594 <a@a.com>
2017-03-25 13:32:28 +0000

    dummy php for loop test

--- services.php
+++ services.php
@@ -3,18 +3,19 @@ if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
     //Pulling
-    $jsondecoded = json_decode($_POST["points"]);
+    //$jsondecoded = json_decode($_POST["points"]);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
     //$data =  $_POST['points'];
 
 
-    foreach($jsondecoded as $item) { //foreach element in $arr
 
-             echo $uses = $item['cop']; 
+    $arr = json_decode('[{"var1":"9","var2":"16","var3":"16"},{"var1":"8","var2":"15","var3":"15"}]');
 
+    foreach($arr as $item) { //foreach element in $arr
+
+      echo $uses = $item['var1']; //etc
     }
-
 
 
 
