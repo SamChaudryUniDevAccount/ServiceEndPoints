4399d4e208b2c9c5da58485c598754f5fa37a949
0805594 <a@a.com>
2017-03-25 15:59:29 +0000

    hard doded version

--- services.php
+++ services.php
@@ -2,19 +2,21 @@
 if (isset($_POST["points"])) {
 
 
-   // $Points = json_decode($_POST["points"]);
+    $Points = json_decode($_POST["points"]);
 
     //Points object shows {"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
-    //echo $Points-> cop;
+    echo $Points-> cop;
+
 
     //Hard coded version -> Works on decode
 
+    //{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
 
-    $json = $_POST["points"];
+   // $json = '{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}';
 
-    $copName = json_decode($json);
+    //$copName = json_decode($json);
 
-    echo $copName->cop;
+    //echo $copName->cop; echos John Maclane
 
 
 
