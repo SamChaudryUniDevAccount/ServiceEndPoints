9d6e005ca7833a7326784ec4b20e281702a6b563
0805594 <a@a.com>
2017-03-25 17:31:35 +0000

    entity decode

--- services.php
+++ services.php
@@ -5,30 +5,8 @@ if (isset($_POST["points"])) {
      //Crate assoctiative array
      $obj = json_decode($_POST["points"]);
 
+     echo $obj;
 
-      $jsonData = stripslashes(html_entity_decode($_POST["points"]));
-
-      $array= (array)json_decode($jsonData,true);
-
-      foreach ($array as $k=>$v){
-
-        echo "Keys are..".$k. "Values are...".$v;
-
-
-     }
-
-
-
-    //$array = (array)json_decode($_POST["points"],true);
-
-    /*
-    foreach ($array as $k=>$v){
-
-        echo "Keys are..".$k. "Values are...".$v;
-
-
-    }
-*/
 
 
 
