cad38839a419ad9054ddb7ba5abac07798ad5d06
0805594 <a@a.com>
2017-03-25 17:12:13 +0000

    array[0] addition

--- services.php
+++ services.php
@@ -4,11 +4,11 @@ if (isset($_POST["points"])) {
     //Cast into array and is working!!!
     $array = (array)json_decode($_POST["points"],true);
 
-    foreach ($array as $k=>$v){
+    foreach ($array[0] as $k=>$v){
 
         echo "Keys are..".$k. "Values are...".$v;
 
-        
+
     }
 
 
