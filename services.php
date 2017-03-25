ec80a8421abf2c8397346c534dc92ef81e41f8f3
0805594 <a@a.com>
2017-03-25 14:21:16 +0000

    donut example

--- services.php
+++ services.php
@@ -30,7 +30,7 @@ if (isset($_POST["points"])) {
 
     $data = json_decode($_POST["points"]);
 
-    echo $data["points"]["cop"];
+    //echo $data["points"]["cop"];
     echo $data["points"]["cop"]->cop;
 
 
