62933ec2ef213bed86ffb91cf7e3f43ec0447025
0805594 <a@a.com>
2017-03-25 16:00:42 +0000

    hard doded version

--- services.php
+++ services.php
@@ -5,8 +5,7 @@ if (isset($_POST["points"])) {
     $Points = json_decode($_POST["points"]);
 
     //Points object shows {"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
-    echo $Points-> cop;
-
+    echo $Points;
 
     //Hard coded version -> Works on decode
 
@@ -17,8 +16,6 @@ if (isset($_POST["points"])) {
     //$copName = json_decode($json);
 
     //echo $copName->cop; echos John Maclane
-
-
 
 
 
