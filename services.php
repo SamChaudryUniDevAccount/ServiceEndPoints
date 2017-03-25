a39d76ab217414220a78f8f2f7a7678139efba44
0805594 <a@a.com>
2017-03-25 13:23:55 +0000

    for loop showing bracket

--- services.php
+++ services.php
@@ -3,17 +3,14 @@ if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
     //Pulling
-    $jsondecoded = json_decode($_POST["points"]);
+    //$jsondecoded = json_decode($_POST["points"]);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
     //$data =  $_POST['points'];
 
     //For loop through the points
-    // echo ("".$points);
+     //echo ("".$points);
 
-   // $array = array('key1' => 'value1', 'key2' => 'value2');
-
-    //echo $obj['points'][i]['cop'];
 
     $obj = json_decode($_POST["points"], TRUE);
 
