a83fb4d52b75ff595a087b86b80961f55491e044
0805594 <a@a.com>
2017-03-25 14:36:00 +0000

    donut example

--- services.php
+++ services.php
@@ -3,7 +3,7 @@ if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
     //Pulling
-    $data = json_decode($_POST["points"],true);
+   echo $data = json_decode($_POST["points"],true);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
     //$data =  $_POST['points'];
