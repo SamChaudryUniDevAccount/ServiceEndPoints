f2b1ff04df9286cb5e3202004c922ac39ea0206e
0805594 <a@a.com>
2017-03-25 14:03:30 +0000

    javascript adjustments and services

--- services.php
+++ services.php
@@ -3,18 +3,26 @@ if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
     //Pulling
-    $data = json_decode($_POST["points"],true);
+    //$data = json_decode($_POST["points"],true);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
     //$data =  $_POST['points'];
 
     //echo $jsondecoded;
 
-    $arr = json_decode($data["points"], true);
+    //$arr = json_decode($data["points"], true);
 
-    echo "Values echoed are". $String = $arr[0];
+    //echo "Values echoed are". $String = $arr[0];
 
+    $json = '
+{
+    "type": "donut",
+    "name": "Cake"
+}';
 
+    $yummy = json_decode($json);
+
+    echo $yummy->type; //donut
 
 
 
