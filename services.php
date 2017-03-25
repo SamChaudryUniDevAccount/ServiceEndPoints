203c334de3a3c09b9f126e894424b067a74cd932
0805594 <a@a.com>
2017-03-25 14:34:18 +0000

    donut example

--- services.php
+++ services.php
@@ -3,7 +3,7 @@ if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
     //Pulling
-    //$data = json_decode($_POST["points"],true);
+    $data = json_decode($_POST["points"],true);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
     //$data =  $_POST['points'];
@@ -15,18 +15,17 @@ if (isset($_POST["points"])) {
     //echo "Values echoed are". $String = $arr[0];
 
 
+     echo   $data->{'cop'};
 
-    $json = '
-{
-   
-    "type": "donut",
-    "name": "Cake"
-}';
+
+/*
+    $json = '{"type": "donut",
+    "name": "Cake"}';
 
     $yummy = json_decode($json);
 
     echo $yummy->type; //donut
-
+*/
 
 
 
