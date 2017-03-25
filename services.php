80806b2d24ca3e59a94d89416305e3560d0467b7
0805594 <a@a.com>
2017-03-25 12:59:20 +0000

    addition olf for each loop

--- services.php
+++ services.php
@@ -6,10 +6,17 @@ if (isset($_POST["points"])) {
     $points = json_decode($_POST["points"]);
 
     //You can for loop through this and grab the values by key value pairs and get what you want
-    $data =  $_POST['points'];
+    //$data =  $_POST['points'];
 
     //For loop through the points
-    echo ("".$points);
+  //  echo ("".$points);
+
+    foreach($points as $obj){
+
+        echo $obj->name;
+
+    }
+
 
     // Access our object's data and array values.
     //Extract values and write to variables
