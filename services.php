5a021d5163356e4d69e1e77b602ef52f9b2b9cfa
0805594 <a@a.com>
2017-03-25 15:38:57 +0000

    hard doded version

--- services.php
+++ services.php
@@ -1,12 +1,12 @@
 <?php
 if (isset($_POST["points"])) {
 
+     $json = json_decode($_POST["points"]);
 
-     //$json = json_decode($_POST["points"]);
+     $copName = $json -> cop;
 
-     //$cop =  $json["points"]["cop"]->cop;
+     echo $copName->cop;
 
-     //echo $cop;
 
 
 
@@ -14,11 +14,11 @@ if (isset($_POST["points"])) {
 
     //{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
 
-    $json = '{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}';
+    //$json = '{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}';
 
-    $copName = json_decode($json);
+    //$copName = json_decode($json);
 
-    echo $copName->cop;
+    //echo $copName->cop; echos John Maclane
 
 
 
