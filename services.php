5905915017c6e2c7e8a617a154be695fe5e13ea2
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-26 16:09:07 +0100

    xx

--- services.php
+++ services.php
@@ -4,7 +4,7 @@ if (isset($_POST["points"])) {
     // Decode our JSON into PHP objects we can use
     ///{"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
 
-    $points = json_decode($_POST["points"],true);
+    $points = json_decode($_POST["points"]);
 
     $stringJSON = $_POST["points"];
 
