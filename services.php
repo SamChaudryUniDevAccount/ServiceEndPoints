3241fc0933fad4addb26c8730f9b45df20342514
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-27 10:06:14 +0100

    echo points put in

--- services.php
+++ services.php
@@ -2,17 +2,15 @@
 if (isset($_POST["points"])) {
 
     // Decode our JSON into PHP objects we can use
+
     /// $points echo {"cop":"John Mclane","airman":"Maverick","wing":"Iceman","rock":"Rocky Balboa"}
+
     ////echo $points->cop; Not working
 
 
     $points = json_decode($_POST["points"],true);
 
-
-    foreach ($points as $key => $value) {
-
-        echo "We are in the for loop".$key;
-    }
+    echo $points;
 
 
 
